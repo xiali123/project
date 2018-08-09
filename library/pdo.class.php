4a9372504b087629b374ce012cd521f5206965d8
xiali <xiali@4399inc.com>
2018-08-09 09:07:32 +0800

    Update 增加pdo类文件大致功能函数

--- library/pdo.class.php
+++ library/pdo.class.php
@@ -1,1 +1,34 @@
 <?php
+class Pdo{
+    function __construct() {
+
+    }
+
+    public function selectDb() {
+
+    }
+
+    public function insert() {
+
+    }
+
+    public function select() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+
+    public function alterTable() {
+
+    }
+
+    public function __call() {
+
+    }
+}
