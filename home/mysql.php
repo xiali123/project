387def3a36823ed17fca11507f3f52ca84a775bd
xiali <xiali@4399inc.com>
2018-08-10 19:34:08 +0800

    Add 增加文件

--- home/mysql.php
+++ home/mysql.php
@@ -1,4 +1,15 @@
 <?php
 
-$smarty->display("mysql.html");
+$act = $_GET['act'];
+if($act == "install") {
+    $dbname = $_POST['dbname'];
+    $sqlname = $_POST['sqlname'];
+    $sqlpwd = $_POST['sqlpwd'];
+    $name = $_POST['name'];
+    $pwd = $_POST['pwd'];
+
+
+} else {
+    $smarty->display("mysql.html");
+}
 die();
