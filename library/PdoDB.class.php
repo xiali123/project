bf3ae0270c31b737e5d79a4ff1cde67c3eba55c6
xiali <xiali@4399inc.com>
2018-08-10 10:21:43 +0800

    Add 增加文件

--- library/PdoDB.class.php
+++ library/PdoDB.class.php
@@ -64,30 +64,6 @@ class PdoDB{
         return true;
     }
 
-    private function reconnect() {
-        try{
-            $this->pdo = new PDO(
-                $this->dns,
-                $this->dbuser,
-                $this->dbpass,
-                array(
-                    PDO::ATTR_PERSISTENT => $this->pconnect //长连接配置
-                )
-            );
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //开启异常处理
-        }catch (PDOException $e){
-            echo "数据库连接失败：".$e->getMessage();
-            return false;
-        }
-        $this->alive = true;
-        return true;
-    }
-
-    private function query($sql) {
-        $sth = $this->pdo->prepare($sql);
-        return $sth;
-    }
-
     public function insert(array $insert_arr, $table = '') {
 
     }
@@ -126,4 +102,78 @@ class PdoDB{
         }
         return $result;
     }
+
+
+    /***************************私有方法开始***********************************/
+    private function reconnect() {
+        try{
+            $this->pdo = new PDO(
+                $this->dns,
+                $this->dbuser,
+                $this->dbpass,
+                array(
+                    PDO::ATTR_PERSISTENT => $this->pconnect //长连接配置
+                )
+            );
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //开启异常处理
+        }catch (PDOException $e){
+            echo "数据库连接失败：".$e->getMessage();
+            return false;
+        }
+        $this->alive = true;
+        return true;
+    }
+
+    private function query($sql) {
+        $sth = $this->pdo->prepare($sql);
+        return $sth;
+    }
+
+    private function getInsertStr(array $insert_arr){
+        if(empty($insert_arr)) {
+            return false;
+        }
+        $str_field = "";
+        $str_v = "";
+        foreach($insert_arr as $key => $value) {
+            $str_field .= ",{$key}";
+            $v_type = gettype($value);
+            if($v_type == "string") {
+                $str_v1 = '"'.$value.'"';
+            }
+            $str_v .= ','.$str_v1;
+        }
+        $str_v = "(".trim($str_v, ',').")";
+        $str_field = "(".trim($str_field, ',').")";
+        $result = array();
+        $result['field'] = $str_field;
+        $result['value'] = $str_v;
+        return $result;
+    }
+
+    private function getInsertSql(array $insert_arr, $table, $type = 1) {
+        if(empty($insert_arr) || empty($table)) {
+            return false;
+        }
+
+        if($type == 1) {
+            $sql_str_arr = array();
+            foreach($insert_arr as $k => $v) {
+                $sql_str_arr[] = $this->getInsertStr($v);
+            }
+
+            $str_sql = "insert into ".$table." ({$sql_str_arr[0]['field']}) value";
+            foreach($sql_str_arr as $k => $v) {
+                $str_sql .= $v['value'].",";
+            }
+            $str_sql = trim($str_sql, ",");
+        } else {
+            $sql_str_arr = $this->getInsertStr($insert_arr);
+            $str_sql = "insert into ".$table." ({$sql_str_arr[0]['field']}) value".$sql_str_arr['value'];
+        }
+
+        return $str_sql;
+    }
+
+    /***************************私有方法结束***********************************/
 }
