edbcf01edbc71424e8b4382bb9ebf801e4767a3c
xiali <xiali@4399inc.com>
2018-08-11 13:16:13 +0800

    Add 增加文件

--- home/index.php
+++ home/index.php
@@ -3,6 +3,7 @@
 $items = array(
     0 => array(
         'name' => "学习",
+        'url'  => "http://www.baidu.com",
     ),
     1 => array(
         'name' => "学习",
@@ -15,8 +16,28 @@ $items = array(
     ),
 );
 
+$items_second = array(
+    0 => array(
+        'name' => "学习",
+        'url'  => "http://www.baidu.com",
+    ),
+    1 => array(
+        'name' => "学习",
+        'url'  => "http://www.hao123.com",
+    ),
+    2 => array(
+        'name' => "学习",
+        'url'  => "http://www.sina.com",
+    ),
+    3 => array(
+        'name' => "学习",
+        'url'  => "http://www.sina.com",
+    ),
+);
+
 $firstSrc = "home/learn/index.php";
 $smarty->assign('items', $items);
+$smarty->assign("items_second", $items_second);
 $smarty->assign('firstSrc', $firstSrc);
 $smarty->display('index.html');
 die;
