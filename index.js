8a59acf8e2b8149262deb4323eaceadf51912a56
xiali <xiali@4399inc.com>
2018-10-09 10:27:53 +0800

    Add 增加文件

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var draw;
 function Draw() {
     this.config = {};
     this.canvas;
@@ -11,8 +10,8 @@ function Draw() {
         this.cvsCfg.y = y;
         this.cvsCfg.width = width;
         this.cvsCfg.height = height;
-        this.cvsCfg.centerX = x + this.cvsCfg.width/2;
-        this.cvsCfg.centerY = x + this.cvsCfg.height/2;
+        this.cvsCfg.centerX = x + Math.floor(this.cvsCfg.width/2);
+        this.cvsCfg.centerY = x + Math.floor(this.cvsCfg.height/2);
         this.cvsCfg.left = x;
         this.cvsCfg.right = x + width;
         this.cvsCfg.top = y;
@@ -21,22 +20,24 @@ function Draw() {
         canvasDom = document.createElement(this.objDraw);
         canvasDom.style.display = "block";
         canvasDom.style.position = "absolute";
-        canvasDom.style.left = x;
-        canvasDom.style.top = y;
-        canvasDom.style.width = width;
-        canvasDom.style.height = height;
+        canvasDom.style.left = x+"px";
+        canvasDom.style.top = y+"px";
+        canvasDom.style.width = (width*0.8)+"px";
+        canvasDom.style.height = (height*0.8)+"px";
+        canvasDom.style.border = "1px solid #000000";
         dom.appendChild(canvasDom);
         this.canvas = canvasDom;
 
         this.isDraw = 1;
         args = new Array();
-        args[0] = this.cvsCfg.centerX;
-        args[1] = this.cvsCfg.top;
-        args[2] = this.cvsCfg.centerX;
-        args[3] = this.cvsCfg.bottom;
+        args[0] = this.cvsCfg.width/2;
+        args[1] = 0;
+        args[2] = this.cvsCfg.width/2;
+        args[3] = this.cvsCfg.height;
         args[4] = "";
         args[5] = "";
         this.line(args);
+        /*
         args[0] = this.cvsCfg.left;
         args[1] = this.cvsCfg.centerY;
         args[2] = this.cvsCfg.right;
@@ -55,21 +56,24 @@ function Draw() {
         args[3] = this.cvsCfg.bottom;
 
         this.line(args);
+        */
         this.isDraw = 0;
 
     };
     this.dot = function(args) {
-        var x, y, dotDom, style;
+        var x, y, dotDom,dowDom ,style;
         x = args[0];
         y = args[1];
-        style = args[2];
         if(this.isDraw == 1) {
             dowDom = document.createElement(this.objDraw);
             //dowDom.setAttribute('style', style);
             dowDom.style.display = "block";
             dowDom.style.position = "absolute";
-            dowDom.style.left = x;
-            dowDom.style.top = y;
+            dowDom.style.left = x+"px";
+            dowDom.style.top = y+"px";
+            dowDom.style.width = "1px";
+            dowDom.style.height = "1px";
+            dowDom.style.border = "1px solid #000000";
             this.canvas.appendChild(dowDom);
             return true;
         } else {
@@ -90,16 +94,22 @@ function Draw() {
         height = endY - endX;
         xy = Math.abs(Math.floor(width / height));
         yx = Math.abs(Math.floor(height / width));
-        for(i = startX, j = startY; i< endX && j<endY; ) {
+        for(i = startX, j = startY; i<= endX && j<=endY; ) {
             dotArgs[0] = i;
             dotArgs[1] = j;
-            this.dot(dotArgs);
+            this.dot(dotArgs);console.log(this.canvas);
             if(xy > yx) {
                 i += xy;
                 j++;
             }else {
                 j += yx;
                 i++;
+            }
+            if(j > endY && i <= endX) {
+                j = endY;
+            }
+            if(i > endX && j <= endY) {
+                i = endX;
             }
         }
     };
@@ -125,4 +135,3 @@ function Draw() {
     }
 }
 
-draw = new Draw();
