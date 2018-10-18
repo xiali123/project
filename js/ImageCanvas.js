21d14b714a773b58c12b9379a66c64036c9897ab
xiali <xiali@4399inc.com>
2018-10-18 17:56:30 +0800

    Add 增加文件

--- js/ImageCanvas.js
+++ js/ImageCanvas.js
@@ -1,3 +1,27 @@
 function ImageCanvas() {
+    var cvs = {};
+    var cvsStr = "canvas";
+    var myConfig = {
+        'width': 200,
+        'height': 200,
+        'border': 1,
+        'isPadding': false,
+        'padding': "#FFFFFF",
+        'borderWidth': 1,
+        'borderColor': "#000000"
+    };
 
+    this.setConfig = function(cfg) {
+        var i;
+        for(i in cfg) {
+            if(this.myConfig[i] == "undefined" || this.myConfig[i] == "") {
+                this.myConfig[i] = cfg[i];
+            }
+        }
+    };
+
+    this.init = function(divDom) {
+        this.cvs = document.createElement(this.cvsStr);
+
+    }
 }
